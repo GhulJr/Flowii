545d9ea8675fa589d62b015dcf3c7dc3eff2e4a7
Oskar Rek <ghuljr@Oskars-MacBook-Pro.local>
2024-04-17 22:46:08 +0200

    Feat: make iOS app ignore safe area

--- composeApp/src/iosMain/kotlin/ui/theme/Theme.ios.kt
+++ composeApp/src/iosMain/kotlin/ui/theme/Theme.ios.kt
@@ -3,8 +3,11 @@ package ui.theme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
 import platform.Foundation.NSLog
+import platform.UIKit.UIBarMetrics
+import platform.UIKit.UIColor
+import platform.UIKit.UIImage
+import platform.UIKit.UINavigationBar
+import platform.UIKit.UINavigationController
 
 @Composable
-actual fun setupSystemParams(colorScheme: ColorScheme) {
-    NSLog("iOS setup system params")
-}
+actual fun setupSystemParams(colorScheme: ColorScheme) {  }
